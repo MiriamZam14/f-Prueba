a17d15e46b0c2ac6678442d81278d288497ef206
Miriam Zamora <45285795+MiriamZam14@users.noreply.github.com>
2018-12-17 10:25:39 -0600

    a
    a

--- sw.js
+++ sw.js
@@ -1,35 +1,33 @@
-//ASIGNAR NOMBRE Y VERISON DE LA CACHE
-const CACHE_NAME='v1_cache_pwa_prueba';
-
-//archivos cacheables en la aplicación
-//todas las imagenes y estilos
+const CACHE_NAME='cache_pwa_prueba';
 var UrlsToCache=[
   './',
-  './css/styles.css',
-  './img/favicon.png',
-  './img/1.png',
-  './img/2.png',
-  './img/3.png',
-  './img/4.png',
-  './img/5.png',
-  './img/6.png',
-  './img/facebook.png',
-  './img/instagram.png',
-  './img/twitter.png',
-  './img/favicon-1024.png',
-  './img/favicon-128.png',
-  './img/favicon-16.png',
-  './img/favicon-512.png',
-  './img/favicon-384.png',
-  './img/favicon-256.png',
-  './img/favicon-192.png',
-  './img/favicon-96.png',
-  './img/favicon-64.png',
-  './img/favicon-16.png',
+  './css/estilos_index.css',
+  './css/menu.css',
+  './css/footer.css'
+  './img/apple-icon-120x120.png',
+  './img/audio.mpeg',
+  './img/1088_2.mp4'
+  './img/tecISC.jpg',
+  './img/apple-icon-precomposed.png',
+  './img/apple-icon-76x76.png',
+  './img/logoISIC.jpeg',
+  './img/apple-icon-72x72.png',
+  './img/android-icon-192x192.png',
+  './img/favicon-16x16.png',
+  './img/eneit.jpg',
+  './img/isic1.jpg',
+  './img/apple-icon-144x144.png',
+  './img/apple-icon-152x152.png',
+  './img/ms-icon-70x70.png',
+  './img/android-icon-36x36.png',
+  './img/favicon-32x32.png',
+  './img/favicon-96x96.png',
+  './img/apple-icon.png',
+  './img/ms-icon-150x150.png',
+  './img/delfinPro.jpg',
+  './img/android-icon-96x96.png',
 ];
 
-//evento install (de instalación )
-//instalación y guardar en cache los recursos estáticos
 self.addEventListener('install',e=>{
   e.waitUntil(
     caches.open(CACHE_NAME)
@@ -41,9 +39,6 @@ self.addEventListener('install',e=>{
     }).catch(err=>
       console.log('no se ha registrado el cache',err)));
 });
-
-//evento activate activar la aplicación
-//este evento es el que hace que funcione sin conexion
 self.addEventListener('activate',e=>{
   const cacheWhiteList=[CACHE_NAME];
   e.waitUntil(
@@ -51,27 +46,21 @@ self.addEventListener('activate',e=>{
       .then(cacheNames =>{
         return Promise.all(cacheNames.map(cacheName=>{
           if(cacheWhiteList.indexOf(cacheName)===- 1){
-              //borramos los elementos que no necesitamos
               return caches.delete(cacheName);
           }
         })
        );
      })
      .then(()=>{
-       //activa la cache en el dispositivo
         self.clients.claim();
      })
   );
 });
-
-//evento fetch traer desde el internet
-//comprobarra si la url está en cache y si no la solicita por internet
 self.addEventListener('fetch',e=>{
     e.respondWith(
       caches.match(e.request)
         .then(res=>{
           if(res){
-            //devuelvo datos desde caches
             return res;
           }
           return fetch(e.request);
